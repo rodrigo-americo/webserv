ec362492a845611b48a6421cc510a9a7985da1a1
Isadora Ghannam <isa.gha@gmail.com>
2026-06-17 20:39:25 -0300

    adiciona testes schema

--- tests/schema_test/test_string.cpp
+++ tests/schema_test/test_string.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_string.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bruno-valero <bruno-valero@student.42.f    +#+  +:+       +#+        */
+/*   By: ighannam <ighannam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 10:18:08 by ighannam          #+#    #+#             */
-/*   Updated: 2026/06/16 18:42:34 by bruno-valer      ###   ########.fr       */
+/*   Updated: 2026/06/17 14:11:41 by ighannam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,75 +15,187 @@
 
 #include <vector>
 
-schema_result_string test_schema_str_minLenth_fail();
+int test_schema_str_minLength_fail_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().minLength(20).name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Minimal lenght not reached. Must be >= 20"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_minLength_fail_basic", LINE_DATA());
+}
+
+int test_schema_str_minLength_fail_limit()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().minLength(8).name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Minimal lenght not reached. Must be >= 8"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_minLength_fail_limit", LINE_DATA());
+}
+
+int test_schema_str_minLength_pass_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().minLength(2).name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_minLength_pass_basic", LINE_DATA());
+}
+
+int test_schema_str_minLength_pass_limit()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().minLength(7).name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_minLength_pass_limit", LINE_DATA());
+}
+
+int test_schema_str_maxLength_fail_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().maxLength(2).name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Max length overreached. Must be <= 2"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_maxLength_fail_basic", LINE_DATA());
+}
+
+int test_schema_str_maxLength_fail_limit()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().maxLength(6).name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Max length overreached. Must be <= 6"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_maxLength_fail_limit", LINE_DATA());
+}
+
+int test_schema_str_maxLength_pass_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().maxLength(20).name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_maxLength_pass_basic", LINE_DATA());
+}
+
+int test_schema_str_maxLength_pass_limit()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().maxLength(7).name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_maxLength_pass_limit", LINE_DATA());
+}
+
+int test_schema_str_startsWith_pass_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().startsWith("Isa").name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_startsWith_pass_basic", LINE_DATA());
+}
+
+int test_schema_str_startsWith_fail_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().startsWith("dora").name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Prefix incorrect. Must be: dora"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_startsWith_fail_basic", LINE_DATA());
+}
+
+int test_schema_str_startsWith_fail_prefix_greather_than_str()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().startsWith("IsadoraTeste").name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Prefix incorrect. Must be: IsadoraTeste"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_startsWith_fail_prefix_greather_than_str", LINE_DATA());
+}
+
+int test_schema_str_noChars_pass_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().noChars("te").name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_noChars_pass_basic", LINE_DATA());
+}
+
+int test_schema_str_noChars_fail_basic()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().noChars("ted").name("NAME");
+    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Forbidden char found. String must not have: ted"));
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_noChars_fail_basic", LINE_DATA());
+}
+
+int test_schema_str_noChars_pass_empty_str()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().noChars("").name("NAME");
+    schema_result_string expected = schema_result_string::success("Isadora");
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_noChars_pass_empty_str", LINE_DATA());
+}
+
+int test_schema_str_minLength_maxLength_fail()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().minLength(20).maxLength(5).name("NAME");
+    std::vector<schema_error> errs;
+    errs.push_back(schema_error("NAME", "Minimal lenght not reached. Must be >= 20"));
+    errs.push_back(schema_error("NAME", "Max length overreached. Must be <= 5"));
+    schema_result_string expected = schema_result_string::failure(errs);
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_noChars_fail_basic", LINE_DATA());
+}
+
+int test_schema_str_minLength_maxLength_fail_custom_msg()
+{
+    std::string str = "Isadora";
+    schema_string name_schema =  schema::string().minLength(20, "test message: min not reached").maxLength(5, "test message: max not reached").name("NAME");
+    std::vector<schema_error> errs;
+    errs.push_back(schema_error("NAME", "test message: min not reached"));
+    errs.push_back(schema_error("NAME", "test message: max not reached"));
+    schema_result_string expected = schema_result_string::failure(errs);
+    schema_result_string got = name_schema.parse(str);
+    return assert(expected, got, "test_schema_str_minLength_maxLength_fail_custom_msg", LINE_DATA());
+}
 
 int main()
 {
     int failures = 0;
 
-    schema_result_string expected = schema_result_string::failure(schema_error("NAME", "Minimal lenght not reached. Must be >= 20"));
-    failures += assert(expected, test_schema_str_minLenth_fail(), "test_schema_str_minLenth_fail", LINE_DATA());
+    std::cout << "\n=== minLength ===\n";
+    failures += test_schema_str_minLength_fail_basic();
+    failures += test_schema_str_minLength_pass_basic();
+    failures += test_schema_str_minLength_fail_limit();
+    failures += test_schema_str_minLength_pass_limit();
+
+    std::cout << "\n=== maxLength ===\n";
+    failures += test_schema_str_maxLength_fail_basic();
+    failures += test_schema_str_maxLength_pass_basic();
+    failures += test_schema_str_maxLength_fail_limit();
+    failures += test_schema_str_maxLength_pass_limit();
+
+    std::cout << "\n=== startsWith ===\n";
+    failures += test_schema_str_startsWith_pass_basic();
+    failures += test_schema_str_startsWith_fail_basic();
+    failures += test_schema_str_startsWith_fail_prefix_greather_than_str();
+
+    std::cout << "\n=== noChars ===\n";
+    failures += test_schema_str_noChars_pass_basic();
+    failures += test_schema_str_noChars_fail_basic();
+    failures += test_schema_str_noChars_pass_empty_str();
+
+    std::cout << "\n=== minLength + maxLength ===\n";
+    failures += test_schema_str_minLength_maxLength_fail();
+
+    std::cout << "\n=== custom error msg ===\n";
+    failures += test_schema_str_minLength_maxLength_fail_custom_msg();
+    
 	return failures;
 }
-
-schema_result_string test_schema_str_minLenth_fail()
-{
-    std::string str = "Isadora";
-    schema_string name_schema =  schema::string().minLength(20).name("NAME");
-    return name_schema.parse(str);
-}
-
-// int main()
-// {
-//     std::string str = "Isadora";
-//     schema_string name_schema =  schema::string().minLength(20).name("NAME");
-//     schema_result_string res = name_schema.parse(str);
-//     std::cout << res.format();
-
-//     schema_result_int res_int = schema::integer().min(10).name("NAME INT").parse("2");
-//     std::cout << res_int.format();
-
-// 	schema_result_int res_int2 = schema::integer().between(10, 20).name("NAME INT").parse("2");
-//     std::cout << res_int2.format();
-
-//     schema_double double_test = schema::dbl().name("DOUBLE").min(2.35);
-//     schema_result_double res_double = double_test.parse("1.8");
-//     std::cout << res_double.format();
-
-//     schema_bool bool_test = schema::boolean().name("BOOL").add_truthy("ok").truthy();
-//     schema_result_bool res_bool = bool_test.parse("False");
-//     std::cout << res_bool.format();
-
-//     struct functor_refine_no_space : public schema_string::functor_refine
-//     {
-//         functor_refine_no_space() {};
-//         functor_refine* clone() const
-//         {
-//             return new functor_refine_no_space(*this);
-//         }
-//         bool operator()(const std::string& value)
-//         {
-//             for (size_t i = 0; i < value.size(); i++)
-//             {
-//                 if (value[i] == ' ')
-//                     return false;
-//             }
-//             return true;
-//         }
-//     };
-
-//     schema_string no_space = schema::string().refine(new functor_refine_no_space(), "there as spaces");
-//     schema_result_string res_str_refine = no_space.parse("tes te");
-//     schema_result_string res_str_refine2 = no_space.parse("tes te");
-//     std::cout << res_str_refine.format();
-//     std::cout << res_str_refine2.format();
-
-//     int arr[] = {301, 302, 303, 307, 308};
-//     std::vector<int> options(arr, arr + 5);
-//     schema_int oneOf = schema::integer().oneOf(options);
-//     schema_result_int res_one_of = oneOf.parse("3010");
-//     std::cout << res_one_of.format();
-
-
-//     return 0;
-// }
